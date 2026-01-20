a1374341e1351255b09bb54c3a4b9a6586af4c6a
hession1650 <philliphession1@gmail.com>
2026-01-20 12:09:32 -0600

    install node and added supabase file

---
+++ Lebetron/libs/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://fmdvxmgiwaugutyyajjq.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtZHZ4bWdpd2F1Z3V0eXlhampxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0OTc4MDUsImV4cCI6MjA4NDA3MzgwNX0.jE6a00kWIr5PC4CKjT8iMiUF8V7ai1ekJNg1PkuKZHE';
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
